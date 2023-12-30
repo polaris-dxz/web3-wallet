c255df76b9477e76e5c6008baca7be9a215aeba0
duxizhi <duxizhi@ones.ai>
2023-12-31 02:04:36 +0800

    chore: 配置依赖

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+
+// 引入插件
+const NodePolyfillWebpackPlugin = require('node-polyfill-webpack-plugin')
+
+// vant 
+const { VantResolver } = require('unplugin-vue-components/resolvers')
+const ComponentsPlugin = require('unplugin-vue-components/webpack')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new NodePolyfillWebpackPlugin(),
+      ComponentsPlugin.default({ resolvers : [VantResolver()]})
+    ]
+  }
 })
